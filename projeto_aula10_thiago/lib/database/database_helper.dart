4420dae6fc28cd5d3e974ac579fbce8952aca6ee
KenzoFukushima <20241ctb0100081@estudantes.ifpr.edu.br>
2026-07-27 11:40:44 -0300

    fazendo slide 10

---
+++ projeto_aula10_thiago/lib/database/database_helper.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  DatabaseHelper._privateConstructor();
+
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+  static Database? _database;
+  static const int _version = 1;
+  static const String _dbName = 'instagram_db.db';
+
+  Future _createDb(Database db, int version) async{
+    await db.execute('''
+        CREATE TABLE posts 
+          (id INTEGER  PRIMARY  KEY AUTOINCREMENT,
+          title  TEXT NOT NULL,
+          text TEXT NOT NULL,
+          liked INTEGER NOT NULL)''');
+  }
+
+  Future<Database> get database async => _database ??= await _initDatabase();
+
+
+  Future<Database> _initDatabase() async {
+    Directory documentsDir = await getApplicationCacheDirectory();
+    String path = join(documentsDir.path, _dbName);
+    return openDatabase(path, onCreate: _createDb, version: _version);
+  }
+}
--- projeto_aula10_thiago/lib/models/post.dart
+++ projeto_aula10_thiago/lib/models/post.dart
@@ -1,9 +1,11 @@
 class  Post  {
+  int? id;
   String title;
   String text;
   bool liked;
 
   Post({
+    this.id,
     required this.title,
     required this.text,
     this.liked = false,
@@ -12,6 +14,21 @@ class  Post  {
   void like () {
     liked = !liked;
   }
+
+  factory Post.fromMap(Map<String, dynamic> json) => Post(
+    id: json['id'],
+    title: json['title'],
+    text: json['text'],
+    liked: json['liked'] == 0 ? false : true
+  );
+
+  Map<String, dynamic> toMap() => {
+    'id' : id,
+    'title' : title,
+    'text' : text,
+    'liked' : liked ? 1: 0,
+};
+
 }
 
 
